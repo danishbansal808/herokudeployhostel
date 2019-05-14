e38fe3e6fd5fa142a5e4b0076656747849a8c1dd
Danish Bansal <38431950+danishbansal808@users.noreply.github.com>
2019-05-15 05:29:35 +0530

    Back to Connection Nothing Worked

--- routes/connection.js
+++ routes/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 var connection = mysql.createPool({
   connectionLimit: 30,
-  host: '35.184.30.192',
+  host: '127.0.0.1',
   user: 'danish',
   password: '12345678',
-  database: 'test5'
+  database: 'test3'
 });
 module.exports=connection;
