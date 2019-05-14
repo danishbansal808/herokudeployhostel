018ac0c0dc8dfe4479f1cbcc4753181c5a3bee27
Danish Bansal <38431950+danishbansal808@users.noreply.github.com>
2019-05-15 05:05:48 +0530

    Updating Connection Settings 2

--- routes/connection.js
+++ routes/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 var connection = mysql.createPool({
   connectionLimit: 30,
-  host: 'remotemysql.com/:3306',
-  user: 'hpNkrtiEMK',
-  password: ' x4qOZ0mtfc',
-  database: 'hpNkrtiEMK'
+  host: '35.184.30.192',
+  user: 'danish',
+  password: '12345678',
+  database: 'test5'
 });
 module.exports=connection;
