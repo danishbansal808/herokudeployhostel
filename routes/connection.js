1a6084dfc1dfc690e1abff4d1499bed7929aac15
Danish Bansal <38431950+danishbansal808@users.noreply.github.com>
2019-05-30 00:04:53 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 var connection = mysql.createPool({
   connectionLimit: 30,
- 'host: '35.184.30.192',
+  host: '35.184.30.192',
   user: 'danish1',
   password: '00000000',
   database: 'test3'
