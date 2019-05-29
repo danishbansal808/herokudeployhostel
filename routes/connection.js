025a35992c51ab7beb20f9565b179af9daedfd9f
Danish Bansal <38431950+danishbansal808@users.noreply.github.com>
2019-05-30 00:04:17 +0530

    Try to connect googlesql 2nd time

--- routes/connection.js
+++ routes/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 var connection = mysql.createPool({
   connectionLimit: 30,
-  host: '127.0.0.1',
-  user: 'danish',
-  password: '12345678',
+ 'host: '35.184.30.192',
+  user: 'danish1',
+  password: '00000000',
   database: 'test3'
 });
 module.exports=connection;
